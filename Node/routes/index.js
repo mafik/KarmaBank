47d6df2c9936c17bb6b2fa5c327ecc4fcb53b2e0
Marek Rogalski <mafikpl@gmail.com>
2012-04-13 14:18:00 +0200

    Print parsed WSDL as output

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -7,26 +7,30 @@ var parse = require('url').parse;
 
 var soap = require('soap');
 var url = 'http://www.xignite.com/xNASDAQLastSale.asmx?WSDL';
+var description = 'WSDL not parsed yet...';
 
 soap.createClient(url, function(err, client) {
   
-  console.log(JSON.stringify(client.describe(), null, '    '));
-
-  exports.view = function(req, res) {
-    var opts = parse(req.url, true).query;
-    var name = opts.name || undefined;
-    // make query to webservice
-    console.log(name);
-    var args = {name: 'value'};
-    client.MyFunction(args, function(err, result) {
-      console.log(result);
-    });
-    res.render('index', { title: 'View!' });
-  };
-  
+  description = JSON
+    .stringify(client.describe(), null, '    ')
+    .replace(/\n/g, '<br>')
+    .replace(/ /g, '&nbsp;');
 
 });
 
+
+exports.view = function(req, res) {
+  var opts = parse(req.url, true).query;
+  var name = opts.name || undefined;
+  // make query to webservice
+  console.log(name);
+  var args = {name: 'value'};
+  client.MyFunction(args, function(err, result) {
+    console.log(result);
+  });
+  res.render('index', { title: 'View!', footer: description });
+};
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Banq' });
+  res.render('index', { title: 'Banq', footer: description });
 };
